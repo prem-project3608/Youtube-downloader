0a706739562e4ea39a312248a6d22b2f5c84b17a
प्रेम बाबू <144128294+prem-project3608@users.noreply.github.com>
2025-06-15 00:18:21 +0530

    Delete untils directory

--- untils/tiktok.js
+++
@@ -1,9 +0,0 @@
-async function download(url) {
-    return {
-        title: 'TikTok media',
-        thumbnail: '',  // You can implement scraping logic
-        formats: [{ quality: 'HD', url: url, type: 'mp4' }]
-    };
-}
-
-module.exports = { download };
